45ad735d8ac32f8b263ddb015c3b067c6acbd2f3
victor toledo <querynetworking@gmail.com>
2023-04-08 01:41:27 -0300

    creacion de layout y estilos para singup

---
+++ src/layouts/JoinLayout/JointLayout.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Icon, Image } from "semantic-ui-react";
+import styles from "./JoinLayout.module.scss";
+
+export const JointLayout = (props) => {
+
+  const { children } = props;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.topBar}>
+        <Link href="/">
+          <Image src="/images/logo.png" alt="Gaming"/>
+        </Link>
+
+        <Link href="/">
+          <Icon name="close"/>
+        </Link>
+      </div>
+
+      <div className={styles.blockLeft}>{children}</div>
+
+      <div className={styles.blockRight}/>
+    </div>
+  )
+}
--- src/pages/join/sign-up/sign-up.js
+++ src/pages/join/sign-up/sign-up.js
@@ -1,14 +1,16 @@
+import { JointLayout } from "@/layouts";
 import style from "./sign-up.module.scss";
 
 const SignUpPage = () => {
   return (
     <>
+      <JointLayout>
         <div>
-            <h3>Crear Cuenta</h3>
+          <h3>Crear Cuenta</h3>
         </div>
-      
+      </JointLayout>
     </>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
