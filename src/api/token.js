6da000d1c8975fa0d6515644117154713e2b2cc2
victor toledo <querynetworking@gmail.com>
2023-04-10 23:31:40 -0300

    funcion para hacer peticiones autenticadas al servidor authFetch

--- src/api/token.js
+++ src/api/token.js
@@ -1,7 +1,28 @@
 import { ENV } from "@/utils";
+import jwtDecode from "jwt-decode";
 
 export class Token {
     setToken(token) {
         localStorage.setItem(ENV.TOKEN, token);
+    }
+
+    getToken() {
+        return localStorage.getItem(ENV.TOKEN);
+    }
+
+    removeToken() {
+        localStorage.removeItem(ENV.TOKEN);
+    }
+
+    hasExpired(token) {
+        const tokenDecode = jwtDecode(token);
+        const expireDate = tokenDecode.exp * 1000;
+        const currentData = new Date().getTime();
+
+        if (currentData > expireDate ) {
+            return true;
+        }
+
+        return false;
     }
 }
