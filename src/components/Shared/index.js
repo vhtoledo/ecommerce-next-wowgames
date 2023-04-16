30949acf9c61a9cd1426af0936f2177b46def694
victor toledo <querynetworking@gmail.com>
2023-04-16 04:43:55 -0300

    creacion de home

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -1,3 +1,7 @@
 export * from "./Separator";
 export * from "./BasicModal";
 export * from "./Confirm";
+export * from "./Label";
+export * from "./GridGames";
+export * from "./BarTrust";
+export * from "./BannerAd";
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,65 @@
+import { Container } from "semantic-ui-react";
 import { BasicLayout } from "@/layouts";
+import { Home } from "@/components/Home";
+import { Separator, BarTrust, BannerAd } from "@/components/Shared";
+
+const platformsId = {
+  playstation: 1,
+  xbox: 4,
+  nintendo: 5,
+  pc: 3,
+};
 
 const HomePage = () => {
   return (
-    <BasicLayout>
-      <h1 style={{ padding: 100 }}>Estamos en la HOME</h1>
-    </BasicLayout>
-  )
-}
+    <>
+      <BasicLayout>
+        <Home.BannerLastGamePublished />
 
-export default HomePage
+        <Separator heigth={100} />
+
+        <Container>
+          <Home.LatestGames title="Ultimos lanzamientos" />
+        </Container>
+
+        <Separator heigth={100} />
+
+        <BarTrust />
+
+        <Separator heigth={100} />
+
+        <Container>
+          <Home.LatestGames
+            title="PlayStation"
+            limit={3}
+            platformId={platformsId.playstation}
+          />
+        </Container>
+
+        <Separator heigth={100} />
+
+        <BannerAd
+          title="Registrate y obten los mejores precios"
+          subtitle="¡Compara con otros juegos y elige el tuyo!"
+          btnTitle="Entrar ahora"
+          btnLink="/account"
+          image="images/img01.png"
+        />
+
+        <Separator heigth={50} />
+
+        <Container>
+          <Home.LatestGames
+            title="Xbox"
+            limit={3}
+            platformId={platformsId.xbox}
+          />
+        </Container>
+
+        <Separator heigth={100} />
+      </BasicLayout>
+    </>
+  );
+};
+
+export default HomePage;
